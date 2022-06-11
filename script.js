a885407627b7f2e2d515c64a8e319ca15b0bb3a4
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-11 18:27:37 -0500

    created a random array index function that works

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-// Assignment code here
+
 //special chars array
-var passArray = [
-  ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", ">", "=", "?", "@", "~"], //index 0
+//
+var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-"];//, "<", ">", "=", "?", "@", "~", //index 0
 
-  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], // index 1
+var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // index 1
 
-  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-    "W", "X", "Y", "Z"], //index 2
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+//, "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", //index 2
 
-  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-    "w", "x", "y", "z"]// index 3
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+//, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"// index 3
 
-]
 
-// this var uniquePass.join is value of the #password text area
-var uniquePass = [];
+// a randomized order or the password array is final array value of the #password text area
+// var uniquePass = [];
+// uniquePass.push
 
-var passLength = window.prompt("How long do you want your password to be? Type a number here between 8 and 128");
-if (passLength < 8 || passLength > 128) {
-  window.alert("You must choose a value between 8 and 128.");
-  passLength();
+function randomIndex(array) {
+  randElement = Math.floor(Math.random() * array.length);
+  return randElement;
 }
-else {
 
-  function generatePassword() {
+// debugger;
+function generatePassword() {
+  var passLength = window.prompt("How long do you want your password to be? Type a number here between 8 and 128");
+  if (passLength < 8 || passLength > 128) {
+    window.alert("You must choose a value between 8 and 128.");
+    generatePassword();
+  }
+
+
+  else {
+    passLength = parseInt(passLength); //this works to set value to integer.
+
     window.alert("You will need to choose at least one of the following: special characters, numbers, lower case letters, upper case letters");
 
     var isSpChars = window.confirm("Do you want to use special characters(ie, *,&,%,$) ?");
-    if (isSpChars)
 
-      var isNums = window.confirm("Do you want to use numbers?");
-
-
-    var isLowers = window.confirm("Do you want to use lower case letters?");
-
-
-    var isUppers = window.confirm("Do you want to use upper case letters?");
-
-
+    if (isSpChars) {
+      var charStr = " ";
+      for (i = 0; i <= passLength; i++) {
+        charStr += charArray[randomIndex(charArray)];
+      }
+      newArray = (charStr.split(""));
+      console.log(newArray);
+      return newArray;
+    }
 
   }
+  // Math.floor(Math.random * passArray[1].length)
+
+  var isNums = window.confirm("Do you want to use numbers?");
+  if (isNums) {
+    return;
+
+  }
+
+
+  var isLowers = window.confirm("Do you want to use lower case letters?");
+  if (isLowers) {
+
+  }
+
+  var isUppers = window.confirm("Do you want to use upper case letters?");
+  if (isUppers) {
+
+  }
+
+
+
+
+
+
+  //   return uniquePass;// 
 }
+
 
 // Get references to the #generate element, the BUTTON.
 var generateBtn = document.querySelector("#generate");
@@ -60,3 +94,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-"];//, "<", ">", "=", "?", "@", "~", //index 0
+
+var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // index 1
+
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+//, "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", //index 2
+
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+//, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"// index 3
+generatePassword();
