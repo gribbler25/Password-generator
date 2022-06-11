5922b89bd6f23b1e39bdfd0a3d99cc0c3a7d04b4
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-11 10:36:41 -0500

    four arrays completed

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment code here
+//special chars array
+var spChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", ">", "=", "?", "@", "~"];
 
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 // Get references to the #generate element
