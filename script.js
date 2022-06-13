d59b9d548730f4e1a350c3d08e1640e2438963af
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-12 20:56:03 -0500

    finished randomizing functions for all arrays/ MVP

--- script.js
+++ script.js
@@ -1,107 +1,140 @@
 
 //special chars array
-//
-var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-"];//, "<", ">", "=", "?", "@", "~", //index 0
+var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", ">", "=", "?", "@", "~"];   //index 0
 
-var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // index 1
+var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];   // index 1
 
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-//, "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", //index 2
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];  //index 2
 
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-//, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"// index 3
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  // index 3
 
+var newArray = [""];
+var finalArray = [""];
 
-// a randomized order or the password array is final array value of the #password text area
-// var uniquePass = [];
-// uniquePass.push
+//shuffle array fxn??
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+// same can be written as:
+// let t = array[i]; array[i] = array[j]; array[j] = t
+// [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
 
+//function to get a random index number in an array.
 function randomIndex(array) {
   randElement = Math.floor(Math.random() * array.length);
   return randElement;
 }
-
-// debugger;
-function generatePassword() {
-  var passLength = window.prompt("How long do you want your password to be? Type a number here between 8 and 128");
-  if (passLength < 8 || passLength > 128) {
-    window.alert("You must choose a value between 8 and 128.");
-    generatePassword();
+//function that takes in an array and a chosen length(int) as arguments,
+// and adds a new random array of that length to the variable array 'newArray'
+var randomArray = function (array, passLength) {
+  for (i = 0; i < passLength; i++) {
+    newArray += array[randomIndex(array)];
   }
-
-
-  else {
-    passLength = parseInt(passLength); //this works to set value to integer.
-
-    window.alert("You will need to choose at least one of the following: special characters, numbers, lower case letters, upper case letters");
-
-    var isSpChars = window.confirm("Do you want to use special characters(ie, *,&,%,$) ?");
-
-    if (isSpChars) {
-      var charStr = " ";
-      for (i = 0; i <= passLength; i++) {
-        charStr += charArray[randomIndex(charArray)];
-      }
-      newArray = (charStr.split(""));
-      console.log(newArray);
-      return newArray;
-    }
-
-  }
-  // Math.floor(Math.random * passArray[1].length)
-
-  var isNums = window.confirm("Do you want to use numbers?");
-  if (isNums) {
-    return;
-
-  }
-
-
-  var isLowers = window.confirm("Do you want to use lower case letters?");
-  if (isLowers) {
-
-  }
-
-  var isUppers = window.confirm("Do you want to use upper case letters?");
-  if (isUppers) {
-
-  }
-
-
-
-
-
-
-  //   return uniquePass;// 
+  console.log(newArray);
 }
+//function for final array of chosen length taken out of a larger array constructed with user input.
+var randomFinalArray = function (array, passLength) {
+  for (i = 0; i < passLength; i++) {
+    finalArray += array[randomIndex(array)];
+  }
+  console.log(newArray);
+}
+
 
 
 // Get references to the #generate element, the BUTTON.
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password(text area) input
 function writePassword() {
-  //run the function to get the character array choices into array and for loop?
+  function generatePassword() {
+
+    var passLength = window.prompt("How long do you want your password to be? Type a number here between 8 and 128");
+    if (passLength < 8 || passLength > 128) {
+      window.alert("You must choose a value between 8 and 128.");
+      generatePassword();
+    }
+    else {
+      passLength = parseInt(passLength); //this works to set value to integer.
+
+      window.alert("You will first choose at least one of the following: SPECIAL CHARACTERS, NUMBERS, LOWER CASE and UPPER CASE letters");
+      var isSpChars = window.confirm("Do you want to use special characters(ie, *,&,%,$) ?");
+
+      if (isSpChars) {
+        randomArray(charArray, passLength);
+      }
+      else {
+        window.confirm("Are you sure you don't want to include special characters?");
+        if (window.confirm) {
+          window.alert("your password will not include special characters");
+        }
+      }
+    }
+
+    var isNums = window.confirm("Do you want to use numbers?");
+    if (isNums) {
+      randomArray(numArray, passLength);
+    }
+    else {
+      window.confirm("Are you sure you don't want to include numbers?");
+      if (window.confirm) {
+        window.alert("Your password will not include numbers");
+      }
+    }
+
+    var isLowers = window.confirm("Do you want to use lower case letters?");
+    if (isLowers) {
+      randomArray(lowerArray, passLength);
+    }
+    else {
+      window.confirm("Are you sure you don't want to include lower case?");
+      if (window.confirm) {
+        window.alert("your password will not include lower case");
+      }
+    }
+
+    var isUppers = window.confirm("Do you want to use upper case letters?");
+    if (isUppers) {
+      randomArray(upperArray, passLength);
+    }
+    else {
+      window.confirm("Are you sure you don't want to include upper case?");
+      if (window.confirm) {
+        window.alert("your password will not include upper case");
+      }
+    }
+    console.log(newArray);
+    randomFinalArray(newArray, passLength);
+    console.log(finalArray);
+    finalArray = Array.from(finalArray);
+
+    // shuffle(newArray);
+    console.log(finalArray);
+
+    return finalArray;
+
+  }
+
   var password = generatePassword();
   // then take the choices made above and put into the text area in the HTML?
 
   //references the password TEXT area
   var passwordText = document.querySelector("#password");
-
+  //the value of the function generatePassword set to 'password' and placed in text box.
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-"];//, "<", ">", "=", "?", "@", "~", //index 0
+// var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-"];//, "<", ">", "=", "?", "@", "~", //index 0
 
-var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // index 1
+// var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // index 1
 
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-//, "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", //index 2
+// var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+// //, "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", //index 2
 
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-//, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"// index 3
-generatePassword();
+// var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+// //, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"// index 3
