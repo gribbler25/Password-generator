08ee990def8fad689130b40480ed993567946cd7
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-07-16 13:10:49 -0500

    refactor to allow text area to be replaced with new password content upon new click of button

--- README.md
+++ README.md
@@ -2,19 +2,14 @@
 
 <img width="1440" alt="Screen Shot 2022-06-12 at 10 30 43 PM" src="https://user-images.githubusercontent.com/98048059/173274750-0e8023c3-4aae-4f29-85f9-11f2bc6a9dac.png">
 
-
-##Languages used:
+## Languages used:
 
     * Javascript/ HTML/ CSS
     * also with the HTML DOM API
 
-##Logic and Challenges
+## Logic and Challenges
 
-    I first thought of this project as consisting of a large object array containing 4 character, number, lower case and upper case arrays respectively and a function that would take in the user's preference of which arrays to include in the password, what the length of the password should be and a randomized large array created with  a `for` loop out of these chosen arrays over which a final function would loop through in a random way to generate the password. 
+    I first thought of this project as consisting of a large object array containing 4 character, number, lower case and upper case arrays respectively and a function that would take in the user's preference of which arrays to include in the password, what the length of the password should be and a randomized large array created with  a `for` loop out of these chosen arrays over which a final function would loop through in a random way to generate the password.
 
     After trying the above, I opted not to make a larger object for the 4 arrays, and instead set them to variables, as the code attempted for looping over an array within an array was not working as intended.  However, when i simply set the code for putting random indices of a determined password length into a new function expression and used that function over each user-chosen array, then used a version of it one last time but set to a new variable for the random array, it worked as intended. I needed to set the final array to a new variable name due to the way I wrote the first random function, specifying building the random indices into a named global array `newArray` which was declared and empty at the beginning of the DOM.  Lastly I used `.join` to eliminate the commas between the letters and characters when they printed to the DOM.
-
-My biggest challenge that I am seeking a solution for is the program's lack of true control over the random choosing from the final array's indices, therefore my inabily to gaurantee that at least one item from each chosen array is a part of the final password array.  More than 80% of the time, however, it does produce this intended result.
-
-
-    
+    * Update: I now realize that the .join turned finalArray into a string, so simply re-declaring that finalArray variable is an empty array at the beginning of the writePassword function served to clear the old password and write a new one to the html so that you can use the button more than once between screen re-fresh.
--- script.js
+++ script.js
@@ -1,15 +1,86 @@
-
 //special chars array
-var charArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", ">", "=", "?", "@", "~"];
+var charArray = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  "-",
+  "<",
+  ">",
+  "=",
+  "?",
+  "@",
+  "~",
+];
 
 var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 var newArray = [];
 var finalArray = [];
+var passwordText = document.querySelector("#password");
 
 //function to get a random index number in an array.
 function randomIndex(array) {
@@ -23,40 +94,45 @@ var randomArray = function (array, passLength) {
     newArray.push(array[randomIndex(array)]);
   }
   console.log(newArray);
-}
+};
 //function for final array of chosen length taken out of a larger array constructed with user input.
 var randomFinalArray = function (array, passLength) {
   for (i = 0; i < passLength; i++) {
     finalArray.push(array[randomIndex(array)]);
   }
   return finalArray;
-}
+};
 
 // Get references to the #generate element, the BUTTON.
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password(text area) input
 function writePassword() {
-  //   var newArray = [];--How to get the generator to clear the value before another is created? this is not the solution.
-  // var finalArray = [];
-  function generatePassword() {
+  finalArray = [];
 
-    var passLength = window.prompt("How long do you want your password to be? Type a number here between 8 and 128");
+  function generatePassword() {
+    var passLength = window.prompt(
+      "How long do you want your password to be? Type a number here between 8 and 128"
+    );
     if (passLength < 8 || passLength > 128) {
       window.alert("You must choose a value between 8 and 128.");
-      generatePassword();
-    }
-    else {
+      return generatePassword();
+    } else {
       passLength = parseInt(passLength); //this works to set value to integer.
 
-      window.alert("You will first choose at least one of the following: SPECIAL CHARACTERS, NUMBERS, LOWER CASE and UPPER CASE letters");
-      var isSpChars = window.confirm("Do you want to use special characters(ie, *,&,%,$) ?");
+      window.alert(
+        "You will first choose at least one of the following: SPECIAL CHARACTERS, NUMBERS, LOWER CASE and UPPER CASE letters"
+      );
+      var isSpChars = window.confirm(
+        "Do you want to use special characters(ie, *,&,%,$) ?"
+      );
 
       if (isSpChars) {
         randomArray(charArray, passLength);
-      }
-      else {
-        window.confirm("Are you sure you don't want to include special characters?");
+      } else {
+        window.confirm(
+          "Are you sure you don't want to include special characters?"
+        );
         if (window.confirm) {
           window.alert("your password will not include special characters");
         }
@@ -66,8 +142,7 @@ function writePassword() {
     var isNums = window.confirm("Do you want to use numbers?");
     if (isNums) {
       randomArray(numArray, passLength);
-    }
-    else {
+    } else {
       window.confirm("Are you sure you don't want to include numbers?");
       if (window.confirm) {
         window.alert("Your password will not include numbers");
@@ -77,8 +152,7 @@ function writePassword() {
     var isLowers = window.confirm("Do you want to use lower case letters?");
     if (isLowers) {
       randomArray(lowerArray, passLength);
-    }
-    else {
+    } else {
       window.confirm("Are you sure you don't want to include lower case?");
       if (window.confirm) {
         window.alert("your password will not include lower case");
@@ -88,8 +162,7 @@ function writePassword() {
     var isUppers = window.confirm("Do you want to use upper case letters?");
     if (isUppers) {
       randomArray(upperArray, passLength);
-    }
-    else {
+    } else {
       window.confirm("Are you sure you don't want to include upper case?");
       if (window.confirm) {
         window.alert("your password will not include upper case");
@@ -99,18 +172,15 @@ function writePassword() {
     console.log(newArray);
     finalArray = randomFinalArray(newArray, passLength);
 
-    console.log(finalArray);
     finalArray = finalArray.join("");
 
+    console.log(finalArray);
     return finalArray;
-
   }
 
   var password = generatePassword();
-  // then take the choices made above and put into the text area in the HTML?
+  // then take the choices made above and put into the text area in the HTML
 
-  //references the password TEXT area
-  var passwordText = document.querySelector("#password");
   //the value of the function generatePassword set to 'password' and placed in text box.
   passwordText.value = password;
 }
